d0aab254db7d382c4bfd38b2d2baddfed1096864
X5464 <rajarshi7474@gmail.com>
2024-06-22 03:38:21 +0530

    Use Xss filter to santize info

--- myApp.js
+++ myApp.js
@@ -2,68 +2,26 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy())
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'deny' }));
 
-app.use(helmet.frameguard({action: 'deny'}))
-
+// Middleware to sanitize input using XSS filter
 app.use((req, res, next) => {
   helmet.xssFilter()(req, res, next);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
+// Serve static files
 app.use(express.static('public'));
 app.disable('strict-transport-security');
+
+const api = require('./server.js');
 app.use('/_api', api);
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(` Usefull programmer Info Security App Started  Started on Port ${port}`);
+  console.log(`Useful programmer Info Security App started on port ${port}`);
 });
