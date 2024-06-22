fccd8b73cbb80063d7a410a8aa03fcc6caffcdbc
X5464 <rajarshi7474@gmail.com>
2024-06-23 01:42:15 +0530

    Use of Nosniff to protect it from MIME sniffing

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
-app.use(helmet.noSniff()));
+app.use(helmet.noSniff());
 
 
 
